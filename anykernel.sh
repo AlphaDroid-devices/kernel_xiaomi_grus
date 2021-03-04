56da55b4c66bb47c46d0195679c020408ec0fcb7
EcrosoftXiao <xy19990903@gmail.com>
2021-03-04 20:12:11 +0800

    Add some scripts for building flashable kernel zip

---
+++ anykernel.sh
@@ -0,0 +1,40 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=kow@lski-custom kernel for Xiaomi Mi 9 SE (Grus)
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=grus
+device.name2=Grus
+device.name3=grus-user
+device.name4=Mi 9 SE
+device.name5=Mi9SE
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/platform/soc/1d84000.ufshc/by-name/boot;
+block_tree=/dev/block/platform/soc/1d84000.ufshc/by-name/dtbo;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel install
+split_boot;
+flash_boot;
+flash_dtbo;
+
+exit 0
+## end install
+
